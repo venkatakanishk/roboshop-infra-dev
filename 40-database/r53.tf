2759a347c13d78ba6dbf4ba9b4f704b959766252
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-03-07 21:13:09 +0530

    changed something

--- 40-database/r53.tf
+++ 40-database/r53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "mongodb" {
   zone_id = var.zone_id
-  name    = "mongodb-${var.environment}-${var.domain_name}"
+  name    = "mongodb-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = "1"
   records = [aws_instance.mongodb.private_ip] # Replace with your actual IP address or other record data
@@ -8,7 +8,7 @@ resource "aws_route53_record" "mongodb" {
 }
 resource "aws_route53_record" "redis" {
   zone_id = var.zone_id
-  name    = "redis-${var.environment}-${var.domain_name}"
+  name    = "redis-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = "1"
   records = [aws_instance.redis.private_ip] # Replace with your actual IP address or other record data
