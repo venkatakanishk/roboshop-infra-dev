f89c1a0636aac074d840e53b9ec6ddb48b472aab
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-03-07 18:15:29 +0530

    changed something

--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "bastion" {
   subnet_id = local.public_subnet_id
   vpc_security_group_ids = [local.bastion_sg_id]
   iam_instance_profile = aws_iam_instance_profile.bastion.name
-  # user_data = file(bastion.sh)
+  user_data = file("bastion.sh")
   root_block_device {
     volume_size = 50
     volume_type = "gp3"
