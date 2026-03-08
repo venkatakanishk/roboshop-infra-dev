5c8a5641885be9adf0bd1f069c927db8c649bec8
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-03-08 22:47:00 +0530

    changed something

--- 40-database/bootstrap.sh
+++ 40-database/bootstrap.sh
@@ -2,9 +2,8 @@
 
 dnf install ansible -y
 component=$1
-environment=$2
 cd /home/ec2-user
 git clone https://github.com/venkatakanishk/ansible-roboshop-roles-tf.git
 cd ansible-roboshop-roles-tf
 git pull
-ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
+ansible-playbook -e component=$component roboshop.yaml
