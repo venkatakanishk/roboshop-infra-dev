24f0363a1b47bec893eb0af6675bc21f8517ab70
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-03-08 17:22:33 +0530

    changed something

--- 40-database/data.tf
+++ 40-database/data.tf
@@ -28,6 +28,6 @@ data "aws_ssm_parameter" "redis_sg_id" {
   name = "/${var.project}/${var.environment}/redis_sg_id"
 }
 
-data "aws_ssm_parameter" "redis_sg_id" {
+data "aws_ssm_parameter" "mysql_sg_id" {
   name = "/${var.project}/${var.environment}/mysql_sg_id"
 }
