24536e2b7ca0f9eda52b2b2a0ab11f3fa38d2619
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-03-08 19:01:19 +0530

    changed something

--- 40-database/iam.tf
+++ 40-database/iam.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "mysql" {
+/* resource "aws_iam_role" "mysql" {
   name = local.iam_role_name #RoboshopDevMysql
 
 
@@ -30,6 +30,52 @@ resource "aws_iam_policy" "mysql" {
   name        = local.mysql_policy_name
   description = "A policy for MySQL Ec2 instance"
   policy      = file("mysql-iam-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "mysql" {
+  role       = aws_iam_role.mysql.name
+  policy_arn = aws_iam_policy.mysql.arn
+}
+
+resource "aws_iam_instance_profile" "mysql" {
+  name = "${var.project}-${var.environment}-mysql"
+  role = aws_iam_role.mysql.name
+}
+ */
+
+resource "aws_iam_role" "mysql" {
+  name = local.iam_role_name #Roboshop-Dev-Mysql
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(
+    {
+        Name = local.iam_role_name
+    },
+    local.common_tags
+  )
+}
+
+resource "aws_iam_policy" "mysql" {
+  name        = local.mysql_policy_name
+  description = "A policy for MySQL Ec2 instance"
+  policy      = templatefile("mysql-iam-policy.json", {
+                environment = var.environment
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "mysql" {
